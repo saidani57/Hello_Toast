5f63d3acb2a87df0fed75fb222afd8b6fb3d1912
Anis <saidanianis763@gmail.com>
2021-10-18 21:45:57 +0100

    Initial commit

---
+++ app/src/main/java/com/example/android/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.android;
+
+import android.content.Intent;
+import android.provider.Settings;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    public static final String extramsg="";
+    public  int mCount = 0;
+    private TextView mShowCount;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mShowCount = (TextView) findViewById(R.id.show_count);
+
+    }
+
+    public void showToast(View view) {
+        Toast toast = Toast.makeText(this, R.string.toast_message,
+                Toast.LENGTH_SHORT);
+        toast.show();
+        Intent intent = new Intent(this , HELLO_TOAST.class);
+
+        String msg =String.valueOf(mCount);;
+        intent.putExtra(extramsg, msg);
+        startActivity(intent);
+    }
+
+    public void countUp(View view) {
+         mCount++;
+        if (mShowCount != null)
+            mShowCount.setText(Integer.toString(mCount));
+
+    }
+}
